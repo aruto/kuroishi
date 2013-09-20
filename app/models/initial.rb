2cdbded8a5f159129c7228cc1856dec93220ad4f
Hokuto Kon <bei_north@yahoo.co.jp>
2013-09-20 14:02:53 +0900

    初期位置テーブルの作成（バリデーション未設定）

---
+++ app/models/initial.rb
@@ -0,0 +1,5 @@
+class Initial < ActiveRecord::Base
+	#validates :lat, 
+	#validates :lng, 
+	#validates :zoom, 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920034228) do
+ActiveRecord::Schema.define(version: 20130920043955) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "initials", force: true do |t|
+    t.float    "lat"
+    t.float    "lng"
+    t.integer  "zoom"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
