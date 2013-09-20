fbd1dafce6056b995684c97c86cad20be9995692
Hokuto Kon <bei_north@yahoo.co.jp>
2013-09-20 14:34:49 +0900

    初期位置のテーブル（仮2）

--- app/models/initial.rb
+++ app/models/initial.rb
@@ -1,5 +1,5 @@
 class Initial < ActiveRecord::Base
-	#validates :lat, 
-	#validates :lng, 
-	#validates :zoom, 
+	validates: lat, presence: true, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
+	validates: lng, presence: true, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+	validates: zoom, 
 end
