04ffac47d8500a69e7af9313e2ac0ec3558843dc
Hokuto Kon <bei_north@yahoo.co.jp>
2013-09-20 10:58:45 +0900

    アプリ作成始め

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kuroishi::Application.config.secret_key_base = 'baa3194b767ac367faeb98a701c6b6ac4ae6f59d2753ca11eb84f7a050da1521e6a434986e9e4381b0f68986e5a0180f89c3890299dbf491b21ad66899a2a7c3'
