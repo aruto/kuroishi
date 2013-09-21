49deb4dfb3286fa23e32268e4b614d5620847262
Hokuto Kon <bei_north@yahoo.co.jp>
2013-09-21 22:56:49 +0900

    initial終わり（多分）

--- app/models/initial.rb
+++ app/models/initial.rb
@@ -1,5 +1,5 @@
 class Initial < ActiveRecord::Base
 	validates :lat, presence: true, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
 	validates :lng, presence: true, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
-	validates :zoom, peresence: true, numericality: {only_integer: true, generater_than: 0}
+	validates :zoom, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
